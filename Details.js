c814d55a43eb45f2d800203f6cd0b3a30d108cbe
Binwant172 <89380679+Binwant172@users.noreply.github.com>
2021-08-24 15:20:02 +0530

    Update Details.js

--- Details.js
+++ Details.js
@@ -7,7 +7,7 @@ toast.configure();
 const details = () => {
 
     const Saved = () =>{
-        toast.success("Your information was saved succesfully!",{
+        toast.success("Your information has been saved succesfully!",{
              position: toast.POSITION.BOTTOM_RIGHT
             // autoClose:10000;
                 
@@ -15,14 +15,19 @@ const details = () => {
     }
     return ( 
         <div className="data">
-            <div className="display-1 bg-success text-white py-5 text-center">Website name
-      <div className=" fs-6 fw-light m-1 text-center text-white">A personalized and fully customizable place made just for helping you with emotional and mental problems including <span className="fw-bold">stress, anxiety, anger and much more</span>. Join us today and change your life forever.</div>
+            <div className="display-1 bg-success text-white py-5 text-center">#Salvation!
+      <div className=" fs-6 fw-light m-1 text-center text-white">A personalized and fully customizable place made just for helping you with emotional and mental problems including <span className="fw-bold">stress, anxiety, anger and much more</span>.<br />Join us today and change your life forever.</div>
       </div>
       <br></br>
 
             <div className="m-4 text-center">
-                <div className="h4 fw-light" id="Sen">What should we call you?   </div><br></br>
-                <input id="inp" className="input px-3 py-2 mt-1" type="text" placeholder="Name"/>
+                <div className="h4 fw-light m-1" id="Sen">What should we call you?   </div><br></br>
+                <input id="inp" className="input px-3 py-2 m-1 mt-1 mb-1" type="text" placeholder="Name"/>
+                <br></br>
+
+                <div className="h4 fw-light mt-3 m-1" id="Sen2">Please type in your E-mail address   </div>
+                {/* <input id="inp" className="input px-3 py-2" type="email" placeholder="E-mail address"/> */}
+                <input type="email" id="email" name="email" className="input px-3 py-2 mt-1 m-1 mb-1" placeholder="E-mail address"></input>
                 <br></br><br></br>
 
                 {/* Gender */}
@@ -40,7 +45,7 @@ const details = () => {
                 </div>
                 <br></br>
                 <Link onClick={Saved} className="btn btn-outline-success btn-lg px-4" data-bs-toggle="offcanvas" to="/mainpage" role="button" aria-controls="offcanvasExample">
-                Next
+                Save
                 </Link>
             </div>
         </div>            
