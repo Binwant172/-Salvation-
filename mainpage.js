27a81af4a90fa6b49646f80961748b76eb95982d
Binwant172 <89380679+Binwant172@users.noreply.github.com>
2021-08-24 15:20:27 +0530

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -1,42 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './mainpage.css';
+// import { Dropdown, DropdownButton,  } from "bootstrap-navbar-dropdown";
 
 const mainpage = () => {
     return (
         
-        <div className="main">
-            <div className="display-1 bg-success text-white py-3 text-center">Website name</div>
-
-
-        {/* <div className="card" id="card-main-div">
-                <div className="card-body">
-                    <h5 className="card-title display-6">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <div className="main">
+            <div id="opts" className="shadow-lg bg-success text-white">
+                <div id="titlebs" className="display-1 fs-1 bg-success text-white px-3 py-3 text-left">#Salvation!</div>
+                        
+                        <Link className="btn btn-success pt-4 pb-4 m-0" id="my-profile" to="/details" role="button">My profile</Link>
+                        <a className="btn btn-success pt-4 pb-4 m-0" id="contact-us" href="mailto:binwantsingh172@gmail.com" role="button">Conatact us</a>
+                        <Link className="btn btn-success pt-4 pb-4 m-0" id="xx">XXXX XX</Link>
+                    </div>
                 </div>
-            </div> */}
-
-<div className="row m-2 my-3">
-  <div className="col-sm-6">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title display-6 text-success">Stress</h5>
-        <p className="card-text">Stress like the mordern day virus. Many people get stressed out over things no matter the age. Let's look at various ways to prevent stress.<br /><br /></p>
-        <Link className="btn btn-success btn-lg p-2" to="#" role="button">Get started</Link>
-      </div>
-    </div>
-  </div>
-  <div className="col-sm-6">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title display-6 text-danger">Anger</h5>
-        <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident quae dolores iure inventore exercitationem, autem consequatur earum reiciendis adipisci modi quis voluptatum vel magni deserunt?</p>
-        <Link className="btn btn-danger btn-lg p-2" to="#" role="button">Get started</Link>
-      </div>
-    </div>
-  </div>
-</div>
-        </div>
      );
 }
  
