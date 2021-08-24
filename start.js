596937e481e6588f530e2e0cee1a65e8fd223a82
Binwant172 <89380679+Binwant172@users.noreply.github.com>
2021-08-24 15:21:02 +0530

    Update start.js

--- start.js
+++ start.js
@@ -3,8 +3,8 @@ import React from 'react';
 const start = () => {
     return ( 
         <div id="main">
-    <div className="display-1 bg-success text-white py-5 text-center">Website name
-    <div className=" fs-6 fw-light m-1 text-center text-white">A personalized and fully customizable place made just for helping you with emotional and mental problems including <span className="fw-bold">stress, anxiety, anger and much more</span>. Join us today and change your life forever.</div>
+    <div className="display-1 bg-success text-white py-5 text-center">#Salvation!
+    <div className=" fs-6 fw-light m-1 text-center text-white">A personalized and fully customizable place made just for helping you with emotional and mental problems including <span className="fw-bold">stress, anxiety, anger and much more</span>.<br /> Join us today and change your life forever.</div>
     </div>
       <br></br>
       </div>
