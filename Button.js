bd4801537a9a5d2a1162f86cef91bd7feee69851
Binwant172 <89380679+Binwant172@users.noreply.github.com>
2021-08-24 15:19:28 +0530

    Update Button.js

--- Button.js
+++ Button.js
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom';
 
 const Button = () => {
     return ( 
+        
         <div className="name">
-
             <div className="text-center"><Link className="btn btn-outline-success btn-lg p-2" to="/details" role="button">Get started</Link></div>
         </div>
      );
